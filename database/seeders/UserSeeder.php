b7140ab6a5e778c5149c7a171238fe8cc68b268e
isnowflakes12-hub <isnowflakes12@gmail.com>
2026-08-21 20:29:18 +0700

    Pembuatan seeder user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,8 +6,37 @@ use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder {
     public function run(): void {
-        User::create(['name'=>'Super Admin','email'=>'superadmin@rshamori.co.id','password'=>Hash::make('admin123'),'role'=>'super_admin','is_active'=>true]);
-        User::create(['name'=>'Admin Marketing','email'=>'marketing@rshamori.co.id','password'=>Hash::make('marketing123'),'role'=>'admin_marketing','is_active'=>true]);
-        User::create(['name'=>'Admin SDM','email'=>'sdm@rshamori.co.id','password'=>Hash::make('sdm123456'),'role'=>'admin_sdm','is_active'=>true]);
+        $users = [
+            [
+                'name' => 'Super Admin IT',
+                'email' => 'superadmin@rshamori.co.id',
+                'role' => 'super_admin',
+                'password' => 'admin123'
+            ],
+            [
+                'name' => 'Admin Marketing',
+                'email' => 'marketing@rshamori.co.id',
+                'role' => 'admin_marketing',
+                'password' => 'marketing123'
+            ],
+            [
+                'name' => 'Admin SDM',
+                'email' => 'sdm@rshamori.co.id',
+                'role' => 'admin_sdm',
+                'password' => 'sdm123456'
+            ]
+        ];
+
+        foreach ($users as $user) {
+            User::updateOrCreate(
+                ['email' => $user['email']],
+                [
+                    'name' => $user['name'],
+                    'password' => Hash::make($user['password']),
+                    'role' => $user['role'],
+                    'is_active' => true
+                ]
+            );
+        }
     }
 }
